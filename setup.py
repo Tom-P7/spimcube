bc35bbf96e776c04b2fd071c632370d300b31845
Tom-P <44195828+samsri93@users.noreply.github.com>
2020-05-22 12:34:36 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='spimexplore',
+      version='1.0',
+      description='Datacube exploration',
+      author='Thomas Pelini',
+      author_email='thomas.pelini@lncmi.cnrs.fr',
+      packages=['MyPackage'],
+      zip_safe=False)
