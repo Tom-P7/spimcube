099992cafa4f6f384c7a5eeb6f390387d5249fb5
Tom-P7 <44195828+Tom-P7@users.noreply.github.com>
2020-05-25 01:05:44 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,15 @@
 from setuptools import setup
 
-setup(name='mexlorpires',
+setup(
+      name='spimcube',
       version='1.0',
-      description='Datacube exploration',
-      author='Tom-P',
-      packages=['spimclass', 'functions'],
+      packages=["spimcube"],
+      
       install_requires=["matplotlib>=3.1", "colorcet"]
-      description="This is a package enabling the exploration of datacube of spectra within a rich GUI interface.")
+      
+      author='Tom-P7',
+      description="This package enables the exploration of datacube of spectra within a rich GUI interface."
+      keywords="spim", "datacube", "hyperspectral"
+      
+      zip_safe=False
+)
