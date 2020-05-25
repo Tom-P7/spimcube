f0937f17ada6e2a374a0de810e33e4704d6be45f
Tom-P7 <44195828+Tom-P7@users.noreply.github.com>
2020-05-25 02:01:39 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,12 +5,14 @@ setup(
       version='1.0',
       packages=['spimcube'],
       license='MIT',
+      download_url='https://github.com/Tom-P7/spimcube/archive/v_1.0.tar.gz',
       
-      install_requires=['matplotlib>=3.1', 'colorcet', 'numpy']
+      install_requires=['matplotlib>=3.1', 'colorcet', 'numpy'],
       
       author="Tom-P7",
-      description="This package enables the exploration of datacube of spectra within a rich GUI interface."
-      keywords=['spim', 'datacube', 'hyperspectral']
+      description="This package enables the exploration of datacube of spectra within a rich GUI interface.",
+      keywords=['spim', 'datacube', 'hyperspectral'],
+      url='https://github.com/Tom-P7/spimcube',
       
-      zip_safe=False
+      zip_safe=False,
 )
