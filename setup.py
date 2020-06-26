b20a9fb2ae9d232bd9dceb88181dfa992854e5c8
Thomas Pelini <44195828+Tom-P7@users.noreply.github.com>
2020-06-26 02:01:15 +0200

    Modify file by upload
    More mature version of setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,27 @@
 from setuptools import setup
 
-setup(name='indev',
-      version='1.3.1',
-      description='My classes and functions',
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(name='spimcube',
+      version='1.3.2',
+      description="This package enables the exploration of datacube of spectra within a rich GUI interface.",
+      keywords=['spim', 'datacube', 'hyperspectral, 3D map'],
+      long_description=long_description,
+      long_description_content_type="text/markdown"
+      url="https://github.com/Tom-P7/spimcube",
+      
       author='Thomas Pelini',
-      author_email='thomas.pelini@lncmi.cnrs.fr',
-      packages=['indev'],
-      zip_safe=False)
+      author_email='thomas.pelini@orange.fr',
+      
+      packages=['spimcube'],
+      python_requires='>=3.6',
+      install_requires=['matplotlib>=3.1', 'colorcet', 'numpy', 'despike'],
+      license='MIT',
+      classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        ]
+      zip_safe=False,
+)
