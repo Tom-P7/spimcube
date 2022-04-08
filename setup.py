fed7552d220abfed1e8f95f04239cd3c4c6344dd
Thomas Pelini <44195828+Tom-P7@users.noreply.github.com>
2022-04-08 18:10:21 +0200

    Update setup.py
    Added some packages dependencies

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(name='spimcube',
       
       packages=['spimcube'],
       python_requires='>=3.6',
-      install_requires=['matplotlib>=3.1', 'colorcet', 'numpy', 'despike'],
+      install_requires=['matplotlib>=3.1', 'numpy', 'scipy', 'pandas', 'colorcet', 'despike', 'lmfit'],
       license='MIT',
       classifiers=[
         "Programming Language :: Python :: 3",
